45cd6fbcd08ae08b47ed1183b91861bfa231afd9
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-06-18 22:00:16 +0300

    alfa 11.1

--- src/userStats/userStats.tsx
+++ src/userStats/userStats.tsx
@@ -4,24 +4,31 @@ import { useEffect, useState } from 'react';
 import { apiProfile } from '../store/reducers/CardsServices';
 import './userStats.scss'
 import { ProfileResponse } from '../store/reducers/interfaces';
+import { useUser } from '@clerk/clerk-react';
+import { log } from 'console';
  
 
 const UserStats: React.FC = () => {
 
     const { data, isSuccess, isFetching, isError, refetch } = apiProfile.useGetAllQuery();
+    const { user } = useUser();
 
     return ( 
         <main className="userStats">
 
-            <h1>Just your stats!</h1>
+            <h1>Leader bord:</h1>
 
             <div className='statsBlock'>
                 {isSuccess && data.map((item, i) => {
+                    
                     return (
-                        <div className="topitem" key={i}>
+                        <div className={`topitem ${item.name === user?.id && 'highlight'}`} key={i}>
+                            <h4>{i+1}. </h4>
                             <img src={item.url} alt="" />
-                            <h3>{item.fullName}</h3>
-                            <h3>{item.elo} elo</h3>
+                            <div className='statsBlockDetails'>
+                                <h4>{item.fullName.slice(0, 16)}</h4>
+                                <h3>{'(' + item.elo + ' elo)'} </h3>
+                            </div>
                         </div>
                     )
                 })}
